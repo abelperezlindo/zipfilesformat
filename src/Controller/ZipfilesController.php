d6101a92b7731a150ce7b941117ecc824f521850
abelpzl <abelpzl@3637004.no-reply.drupal.org>
2024-05-24 14:58:06 -0300

    Add some improvements

--- src/Controller/ZipfilesController.php
+++ src/Controller/ZipfilesController.php
@@ -7,6 +7,7 @@ use Drupal\node\Entity\Node;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\RedirectResponse;
+use Drupal\Core\File\FileSystemInterface;
 
 /**
  * Controller class for download all in zip.
@@ -18,13 +19,8 @@ class ZipfilesController extends ControllerBase {
    */
   public function download(Request $request, $nid, $field_name) {
     $return = $request->query->get('return') ?? '/';
-    $response = new Response();
-    $schema = 'temporary://';
-    $filebasepath = \Drupal::service('file_system')->realpath($schema);
+
     $filename = 'Archivos' . \Drupal::time()->getRequestTime() . '.zip';
-    $realpath = $filebasepath . '/' . $filename;
-    // $imagen = file_get_contents('productos://tv.jpg');
-    // $ruta = file_unmanaged_save_data($image, 'public://tv.jpg', FILE_EXISTS_REPLACE);
 
     // Check if ZipArchive class exists.
     if (!class_exists('ZipArchive')) {
@@ -44,13 +40,17 @@ class ZipfilesController extends ControllerBase {
     elseif ($node->{$field_name}->isEmpty()) {
       return new RedirectResponse($return);
     }
-
+    // Prepare destination directory.
+    $directory = 'public://zipfiles/' . $node->id();
+    $file_system = \Drupal::service('file_system');
+    $file_system->prepareDirectory($directory, FileSystemInterface:: CREATE_DIRECTORY | FileSystemInterface::MODIFY_PERMISSIONS);
+    $zip_filename = $file_system->realpath($directory . '/' . $filename);
     $zip = new \ZipArchive;
-    if ($zip->open($realpath, \ZipArchive::CREATE) === TRUE) {
+    if ($zip->open($zip_filename, \ZipArchive::CREATE) === TRUE) {
       foreach ($node->{$field_name}->referencedEntities() as $file) {
-        $file_reaLpath = str_replace($schema, $filebasepath . '/', $file->getFileUri());
-        if (file_exists($file_reaLpath)) {
-          $zip->addFile($file_reaLpath, $file->getFilename());
+        $field_filepath = $file_system->realpath($file->getFileUri());
+        if (file_exists($field_filepath)) {
+          $zip->addFile($field_filepath, $file->getFilename());
         }
         else {
           \Drupal::messenger()->addMessage("The file {$file->getFilename()} does not exist", 'warning');
@@ -59,13 +59,14 @@ class ZipfilesController extends ControllerBase {
     }
     $zip->close();
 
-    if (file_exists($realpath)) {
+    if (file_exists($zip_filename)) {
+      $response = new Response();
       $response->headers->set('Expires', '0');
       $response->headers->set('Cache-Control', 'must-revalidate, post-check=0, pre-check=0');
       $response->headers->set('Content-Type', 'application/zip');
       $response->headers->set('Content-Disposition', 'attachment; filename="' . $filename . '"');
       $response->headers->set('Content-Transfer-Encoding', 'binary');
-      $response->setContent(file_get_contents($realpath));
+      $response->setContent(file_get_contents($zip_filename));
       return $response;
     }
     return new RedirectResponse($return);
